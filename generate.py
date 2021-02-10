fdad37543d9697f0c1f2b6f8ca3d8c2d85d17f2e
pstrawbr <parker.strawbridge@uvm.edu>
2021-02-10 12:12:24 -0500

    Begin creating a joint

--- generate.py
+++ generate.py
@@ -1,33 +1,35 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    x = 2
+    y = 2
+    z = 0.5
+    pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
+
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    x = 0
+    y = 0
+    z = 0.5
+    pyrosim.Send_Cube(name="Torso", pos=[x, y, z], size=[length, width, height])
+
+    pyrosim.Send_Joint(name="Torso_Leg", parent="Torso", child="Leg", type="revolute", position="0.5,0,1")
+
+    x = 1
+    z = 1.5
+    pyrosim.Send_Cube(name="Leg", pos=[x, y, z], size=[length, width, height])
+
+    pyrosim.End()
+
 
 length = 1
 width = 1
 height = 1
 
-x = 0
-y = 0
-z = 0.5
 
-for a in range(6):
-    x += 1
-    for b in range(6):
-        y += 1
-        for i in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
-            z += (height * 0.5)
-            length = length * 0.9
-            width = width * 0.9
-            height = height * 0.9
-            z += (height * 0.5)
-
-        length = 1
-        width = 1
-        height = 1
-        z = 0.5
-    y = 0
-
-
-
-pyrosim.End()
+Create_World()
+Create_Robot()
