c27216adb77480964e10d4abc9b54b2e930dd53c
pstrawbr <parker.strawbridge@uvm.edu>
2021-02-10 11:05:06 -0500

    Finished monolith and 6x6 towers

--- generate.py
+++ generate.py
@@ -1,7 +1,33 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
+length = 1
+width = 1
+height = 1
+
+x = 0
+y = 0
+z = 0.5
+
+for a in range(6):
+    x += 1
+    for b in range(6):
+        y += 1
+        for i in range(10):
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z], size=[length,width,height])
+            z += (height * 0.5)
+            length = length * 0.9
+            width = width * 0.9
+            height = height * 0.9
+            z += (height * 0.5)
+
+        length = 1
+        width = 1
+        height = 1
+        z = 0.5
+    y = 0
+
+
 
 pyrosim.End()
