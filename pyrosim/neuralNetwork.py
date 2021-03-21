2cc9686b2c285c166d8ed983675d80a4e211bb0d
pstrawbr <parker.strawbridge@uvm.edu>
2021-03-21 13:40:51 -0400

    Finished the closed loop portion of Assingment 7

--- pyrosim/neuralNetwork.py
+++ pyrosim/neuralNetwork.py
@@ -15,7 +15,6 @@ class NEURAL_NETWORK:
         for line in f.readlines():
 
             self.Digest(line)
-
         f.close()
 
     def Print(self):
@@ -33,7 +32,7 @@ class NEURAL_NETWORK:
             if self.neurons[neuronName].Is_Sensor_Neuron():
                 self.neurons[neuronName].Update_Sensor_Neuron()
             else:
-                self.neurons[neuronName].Update_Hidden_Or_Motor_Neuron()
+                self.neurons[neuronName].Update_Hidden_Or_Motor_Neuron(self.neurons, self.synapses)
 
     def Get_Neuron_Names(self):
         return self.neurons.keys()
--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -69,8 +69,15 @@ class NEURON:
     def Update_Sensor_Neuron(self):
         self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
 
-    def Update_Hidden_Or_Motor_Neuron(self):
+    def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
         self.Set_Value(0.0)
+        for key in synapses:
+            if self.Get_Name() == key[1]:
+                self.Allow_Presynaptic_Neuron_To_Influence_Me(synapses[key].Get_Weight(), neurons[key[0]].Get_Value())
+        self.Threshold()
+
+    def Allow_Presynaptic_Neuron_To_Influence_Me(self, weight, presynampticValue):
+        self.Add_To_Value(weight * presynampticValue)
 
 # -------------------------- Private methods -------------------------
 
